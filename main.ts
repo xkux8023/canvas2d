75f759454fb49847eaf161213f3a0ef622f551bb
xkux8023 <326712928@qq.com>
2019-04-29 22:28:24 +0800

    first commit

---
+++ main.ts
@@ -0,0 +1,9 @@
+import { Canvas2D } from './canvas2d/Canvas2D'
+
+let canvas: HTMLCanvasElement | null = document.getElementById('canvas') as HTMLCanvasElement
+if (canvas === null) {
+  alert ("无法获取HTMLCanvasElement !!!")
+  throw new Error("无法获取HTMLCanvasElement !!!")
+}
+let canvas2d: Canvas2D = new Canvas2D(canvas)
+canvas2d.drawText("hello world from module!")
